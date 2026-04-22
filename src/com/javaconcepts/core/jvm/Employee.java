ec7bcea448d4000a9ed8a70bc355951c2325a006
Goutham <gouthamraj020102@gmail.com>
2026-04-22 22:38:16 +0530

    My first Java program

---
+++ src/com/javaconcepts/core/jvm/Employee.java
@@ -0,0 +1,9 @@
+package com.javaconcepts.core.jvm;
+
+public class Employee {
+    public static void main(String[] args) {
+        // this is a comment line, compiler will ignore this line and it will not be executed
+        int a = -10;
+        System.out.println("this is my first java program and output of a is: " + a);
+    }
+}
---
+++ src/com/javaconcepts/core/jvm/Student.java
@@ -0,0 +1,13 @@
+package com.javaconcepts.core.jvm;
+
+import java.util.Arrays;
+
+public class Student {
+    public static void main(String[] args) {
+        // example of JRE
+        int a = Math.abs(-1);
+        int[] arr = {1, 5, 8, 9};
+        Arrays.sort(arr);
+        System.out.println("Value of a is: " + a);
+    }
+}
