fea9f4367e7a1b942d08b0ab16e3b83b036165d3
Goutham <gouthamraj020102@gmail.com>
2026-04-25 13:35:34 +0530

    Types of Conversions

--- src/com/javaconcepts/core/variables/Main.java
+++ src/com/javaconcepts/core/variables/Main.java
@@ -31,5 +31,33 @@ public class Main {
         float var2 = .1f;
         float floatVar = var1 - var2;
         System.out.println(floatVar);
+
+
+        // Widening conversion example
+        byte x = 10;
+        int intVar = x; // implicit casting from byte to int
+        System.out.println(intVar);
+
+        // Narrowing conversion example
+        int y = 100;
+        byte byteVar = (byte) y; // explicit casting from int to byte
+        System.out.println(byteVar);
+
+        // range of byte: -128 to 127
+        int integerVar = 128;
+        byte byteVariable = (byte) integerVar;
+        System.out.println(byteVariable);
+
+        // promotion byte to int
+        byte a = 127;
+        byte b = 1;
+        int sum = a + b;
+        System.out.println(sum);
+
+        // explicit casting from double to int
+        int c = 34;
+        double doubleVar = 20d;
+        int doubleSum = (int) (c + doubleVar);
+        System.out.println(doubleSum);
     }
 }
