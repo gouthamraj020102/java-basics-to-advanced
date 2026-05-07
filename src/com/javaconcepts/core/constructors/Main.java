521f16e002a204efeced2bb6d4d112ea3bd36324
Goutham <92370689+gouthamraj020102@users.noreply.github.com>
2026-05-07 22:27:32 +0530

    Constructors in Java
    * Default constructor
    
    * types of constructors
    
    * Constructor chaining: super keyword

---
+++ src/com/javaconcepts/core/constructors/Main.java
@@ -0,0 +1,14 @@
+package com.javaconcepts.core.constructors;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculation obj1 = Calculation.getInstance();
+        System.out.println(obj1.name); // Output: Gowtham
+        Calculation obj2 = new Calculation("Gowtham Raju");
+        System.out.println(obj2.name); // Output: Gowtham Raju
+        Calculation obj3 = new Calculation("Gowtham Raju", 12345);
+        System.out.println(obj3.name + ", " + obj3.id); // Output: Gowtham Raju, 12345
+
+        Manager manager = new Manager(1001, 24);
+    }
+}
