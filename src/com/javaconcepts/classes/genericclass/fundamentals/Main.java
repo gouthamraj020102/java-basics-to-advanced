aac972c234a2cfa3481e1e7c84b4130571532755
Goutham <gouthamraj020102@gmail.com>
2026-07-06 19:19:53 +0530

    More than one Generic Types

--- src/com/javaconcepts/classes/genericclass/fundamentals/Main.java
+++ src/com/javaconcepts/classes/genericclass/fundamentals/Main.java
@@ -1,5 +1,8 @@
 package com.javaconcepts.classes.genericclass.fundamentals;
 
+import com.javaconcepts.classes.genericclass.Pair;
+import com.javaconcepts.classes.genericclass.Print;
+
 public class Main {
     public static void main(String[] args) {
         // Without Generics need to cast the value to the appropriate type
@@ -17,5 +20,10 @@ public class Main {
         if (printValueWithGenerics == 110) {
             System.out.println("Print value is 110");
         }
+
+        // More than one Generic Type
+        Pair<String, Integer> pair = new Pair<>();
+        pair.put("Hello", 1010);
+        System.out.println("Key: " + pair.getKey() + ", Value: " + pair.getValue());
     }
 }
