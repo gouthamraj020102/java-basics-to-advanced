392118eac8ccdb635781a4bd8db8cf3b9b7de622
Goutham <gouthamraj020102@gmail.com>
2026-07-13 19:50:02 +0530

    Functional Interface in Java

--- src/com/javaconcepts/interfaces/privatemethod/Bird.java
+++ src/com/javaconcepts/interfaces/privatemethod/Bird.java
@@ -3,20 +3,24 @@ package com.javaconcepts.interfaces.privatemethod;
 public interface Bird {
     void canFly();
 
+    // Default Method
     public default void minimumFlyingHeight() {
         myStaticPublicMethod();
         myPrivateMethod();
         myPrivateStaticMethod();
     }
 
+    // Static Method
     static void myStaticPublicMethod() {
         myPrivateStaticMethod();
     }
 
+    // Private Method
     private void myPrivateMethod() {
         System.out.println("Inside Private Method");
     }
 
+    // Private Static Method
     private static void myPrivateStaticMethod() {
         System.out.println("Inside Private Static Method");
     }
