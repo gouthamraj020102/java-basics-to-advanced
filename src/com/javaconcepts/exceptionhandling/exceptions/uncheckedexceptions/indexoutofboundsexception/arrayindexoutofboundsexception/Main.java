059bdae3408a5d178ec9fccce45d34d3778d4c59
Goutham <gouthamraj020102@gmail.com>
2026-07-29 22:00:47 +0530

    UnChecked or Runtime Exceptions in Java

---
+++ src/com/javaconcepts/exceptionhandling/exceptions/uncheckedexceptions/indexoutofboundsexception/arrayindexoutofboundsexception/Main.java
@@ -0,0 +1,9 @@
+package com.javaconcepts.exceptionhandling.exceptions.uncheckedexceptions.indexoutofboundsexception.arrayindexoutofboundsexception;
+
+public class Main {
+    public static void main(String[] args) {
+        // 3.1 ArrayIndexOutOfBoundsException
+        int[] val = new int[2];
+        System.out.println(val[3]);
+    }
+}
---
+++ src/com/javaconcepts/exceptionhandling/exceptions/uncheckedexceptions/nullpointerexception/Main.java
@@ -0,0 +1,9 @@
+package com.javaconcepts.exceptionhandling.exceptions.uncheckedexceptions.nullpointerexception;
+
+public class Main {
+    public static void main(String[] args) {
+        // 4. NullPointerException
+        String val = null;
+        System.out.println(val.charAt(0));
+    }
+}
